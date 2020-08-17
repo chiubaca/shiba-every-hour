7c22e3e289882a9fb5b72ef6907729aae34a2e1e
Alex Chiu <alexchiu11@gmail.com>
2020-08-17 22:16:51 +0100

    Tidy up 🧹

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const Twitter = require('twitter');
-const fetch = require('node-fetch');
-const request = require('request').defaults({ encoding: null });
+const Twitter = require("twitter");
+const fetch = require("node-fetch");
+const request = require("request").defaults({ encoding: null });
 
 if (process.env.NODE_ENV === "development") {
-  require('dotenv').config()
+  require("dotenv").config()
 }
 
 const client = new Twitter({
@@ -15,18 +15,18 @@ const client = new Twitter({
 
 function sendImageToTwitter(imageBuffer) {
   // Adapted from this example - https://github.com/desmondmorris/node-twitter/tree/master/examples#media
-  client.post('media/upload', { media: imageBuffer }, (error, media) => {
+  client.post("media/upload", { media: imageBuffer }, (error, media) => {
     if (error) {
       console.error("Something went wrong uploading image...😫", error)
     } else {
-      client.post('statuses/update', { status: "", media_ids: media.media_id_string })
-        .then(resp => console.log("🐕‍🦺New shibe successfully posted 🐕‍🦺", resp))
+      client.post("statuses/update", { status: "", media_ids: media.media_id_string })
+        .then(resp => console.log("🐕‍🦺New shibe successfully posted!🐕‍🦺"))
         .catch(error => console.error("Thats ruff, something went wrong posting to Twitter...😒", error))
     }
   });
 }
 
-fetch('http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true')
+fetch("http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true")
   .then((res) => res.json())
   .then((json) => json[0]) 
   .then((imageURL) => {
@@ -34,4 +34,4 @@ fetch('http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true')
       sendImageToTwitter(body);
     });
   })
-  .catch((error) => console.error('There was an error fetching a shibe...😑', error));
+  .catch((error) => console.error("There was an error fetching a shibe...😑", error));
