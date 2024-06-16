2e7d101a4544b6c5175b9b9a0a01a8de0ab4731f
Alex Chiu <alexander.chiu@houseful.co.uk>
2024-06-16 21:23:41 +0100

    fix: correct env naming

--- src/uploadToTwitter.mjs
+++ src/uploadToTwitter.mjs
@@ -3,20 +3,20 @@ import { TwitterApi } from "twitter-api-v2";
 const twitterClient = new TwitterApi({
   appKey: process.env.TWITTER_API_KEY,
   appSecret: process.env.TWITTER_API_KEY_SECRET,
-  accessToken: process.env.ACCESS_TOKEN,
-  accessSecret: process.env.ACCESS_TOKEN_SECRET,
+  accessToken: process.env.TWITTER_ACCESS_TOKEN,
+  accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
 export const uploadToTwitter = async (imgBuffer) => {
   try {
     const mediaKey = await twitterClient.v1.uploadMedia(imgBuffer, {
-      mimeType: "jpg",
+      type: "jpg",
     });
 
     const tweet = await twitterClient.v2.tweet({
       media: { media_ids: [mediaKey] },
     });
-    console.log("✅ Successly posted to twiiter!", tweet);
+    console.log("✅ Successfully posted to twiiter!", tweet);
   } catch (error) {
     console.error("❌ Problem uploading media to twitter", error);
     throw new Error("Problem uploading media to twitter");
